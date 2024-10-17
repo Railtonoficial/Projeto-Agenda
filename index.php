53aebf8ca13abb28314592b9fae68b520c02141f
Railton Araujo <railtonaraujoofc@gmail.com>
2024-10-17 11:53:20 -0300

    Inciando um projeto de agendamento

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo-padrão.css">
+    <title>Sistema Agenda 1.0</title>
+</head>
+
+<body>
+<header>
+    <h1>Sistema Agenda 1.0</h1>
+    <nav>
+        <a href="#">Home</a> |
+        <a href="#">Contato</a> |
+        <a href="#">Tarefas</a> |
+        <a href="#">Eventos</a>
+    </nav>
+</header>
+<mian>
+
+</mian>
+</body>
+</html>
